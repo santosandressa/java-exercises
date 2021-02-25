ec4ce78794e00882059a77eac93bc9c7f46cd9dc
Andressa Santos <andressasantosp0@gmail.com>
2021-02-25 12:49:07 -0300

    25/02

---
+++ ProjetoJavaGeneration/PacoteJava/qualMaior.java
@@ -0,0 +1,28 @@
+package PacoteJava;
+
+import java.util.Scanner;
+
+public class qualMaior {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner ler = new Scanner(System.in);
+		
+		int num1,num2,num3;
+		
+		System.out.println("Digite o primeiro número: ");
+		num1 = ler.nextInt();
+		System.out.println("Digite o segundo número: ");
+		num2 = ler.nextInt();
+		System.out.println("Digite o terceiro número: ");
+		num3 = ler.nextInt();
+		
+		if(num1 > num2 && num1> num3) {
+			System.out.printf("Número: %d, é maior ", num1);
+		} else if (num2 > num1 && num2 > num3) {
+			System.out.printf("Número: %d, é maior ", num2);
+		} else if (num3 > num1 && num3 > num2){
+			System.out.printf("Número: %d, é maior ", num3);
+		}
+	}
+}
